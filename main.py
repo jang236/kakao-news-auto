4a2a8aff55c8c507b2feae18620cc049113bbd6a
jang236 <myfreelove12@gmail.com>
2026-03-27 10:08:15 +0900

    fix: .replit port 8080, python -m uvicorn

--- main.py
+++ main.py
@@ -345,5 +345,5 @@ async def reset_db():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
