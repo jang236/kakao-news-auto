d2f62da4b0e52b80f1653287891c07138ee48192
jang236 <myfreelove12@gmail.com>
2026-03-27 15:50:36 +0900

    fix: revert all ports to 8000, python -m uvicorn

--- main.py
+++ main.py
@@ -341,5 +341,5 @@ async def reset_db():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
